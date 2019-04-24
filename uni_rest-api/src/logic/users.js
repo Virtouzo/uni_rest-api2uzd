654fef827c9c46bb95d65b21a07d906e40eecbed
Virtouzo-Laptop <rokastulaba@gmail.com>
2019-04-24 14:36:48 +0300

    getting with array param

---
+++ uni_rest-api/src/logic/users.js
@@ -0,0 +1,31 @@
+const req = require("request-promise");
+const Promise = require("bluebird");
+const _ = require("lodash");
+
+function attachItemsToUser(user) {
+	const itemIds = user.items.map(item => item.id);
+
+	return Promise.try(() => {
+		const gennedParams = "&foo[]=" + itemIds.join("&foo[]=");
+		return req({
+			uri: `http://shop:3001/shop/multiple/?${gennedParams}`,
+			json: true
+		});
+	})
+		.then(body => {
+			console.log("received resp");
+			console.dir(body);
+			if (body.error) throw new Error(body.message);
+			console.log("received items resp: ");
+			console.dir(body);
+			user.items = body;
+			return user;
+		})
+		.catch(function(resp) {
+			throw resp.error;
+		});
+}
+
+module.exports = {
+	attachItemsToUser
+};
